851bea4a95b1110eb8b5c6f4278d05610ebae048
Effex <jtcartled@gmail.com>
2025-09-28 10:55:34 +0800

    Add files via upload

--- src/app/ImageCard.jsx
+++ src/app/ImageCard.jsx
@@ -18,10 +18,10 @@ export default function ImageCard({ title, image, altText, href }) {
 
       <div className="absolute bottom-0 flex flex-row flex-1 justify-between w-full p-4 rounded-bl-4xl">
         <h1 className="text-3xl font-light text-black">{title}</h1>
-          <div className="flex justify-between items-center bg-stone-200 border border-black/20 rounded-3xl w-20 p-1 text-black text-sm">
-            <a href={href}><p className="pl-2">Visit</p></a>
-          <div className="bg-green-700 w-2 h-2 mr-1 rounded-full"></div>
-      </div>
+          <a href={href}><div className="flex justify-between items-center bg-stone-200 border border-black/20 rounded-3xl w-20 p-1 text-black text-sm">
+            <p className="pl-2">Visit</p>
+            <div className="bg-green-700 w-2 h-2 mr-1 rounded-full"></div>
+          </div></a>
       </div>
     </div>
   );
