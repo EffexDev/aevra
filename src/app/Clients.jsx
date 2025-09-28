d6883b8c05dda87d08f00d88f62e56181edfa06a
Effex <jtcartled@gmail.com>
2025-09-28 10:49:26 +0800

    Add files via upload

--- src/app/Clients.jsx
+++ src/app/Clients.jsx
@@ -5,7 +5,7 @@ export default function CallToAction() {
     <div className="flex flex-col w-full pt-20">
       <h2 className="text-3xl text-center mb-10">Built by Aevra</h2>
       <div className="flex flex-col justify-evenly items-center lg:flex-row lg:align-top lg:pl-30 lg:pr-30">
-        <ImageCard title="MyTek IT Services" image="/mytekss.jpg" altText="Wintery wonderland" href="https://www.mytek.net.au"/>
+        <ImageCard title="MyTek IT Services" image="/mytekSS.jpg" altText="MyTek IT Services" href="https://www.mytek.net.au"/>
         <ImageCard title="MyTek IT Services" image="/winterlandscape.jpg" altText="Wintery wonderland" href="https://www.mytek.net.au"/>
       </div>
     </div>
